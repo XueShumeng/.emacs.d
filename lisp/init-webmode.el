a066d7ce44bbcaa3469ff042044f227d272c54b7
xueshumeng <xueshumeng@jetair.com.cn>
2019-08-25 09:44:21 +0800

    Add emmet mode

--- lisp/init-webmode.el
+++ lisp/init-webmode.el
@@ -6,5 +6,12 @@
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode)))
 
+(when (maybe-require-package 'emmet-mode)
+  ;; Auto-start on any markup modes
+  (add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'html-mode-hook 'emmet-mode)
+  (add-hook 'css-mode-hook  'emmet-mode))
+
 (provide 'init-webmode)
 ;;; init-webmode.el ends here
