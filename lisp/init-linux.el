6a4177d2fee109ff281b6a08bae7d880c4b2b245
xueshumeng <xue.shumeng@yahoo.com>
2021-05-15 15:43:59 +0800

    新增Linux环境配置

--- lisp/init-linux.el
+++ lisp/init-linux.el
@@ -2,6 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
+(when (window-system)
+  (toggle-frame-fullscreen)
+  (setf x-super-keysym 'meta))
+
+(when window-system
+  (let ((monaco-font "-*-Monaco-*-*-*-*-12-*-*-*-*-*-*-*")
+	(source-code-pro "-*-Source Code Pro-*-*-*-*-15-*-*-*-*-*-*-*")
+	(source-code-variable "-*-SourceCodeVariable-*-*-*-*-15-*-*-*-*-*-*-*")
+	(fira-code "-*-Fira Code-*-*-*-*-14-*-*-*-*-*-*-*")
+	(sarasa-font "-*-Sarasa Mono SC-*-*-*-*-12-*-*-*-*-*-*-*"))
+    (set-face-attribute 'default nil :font source-code-variable)))
+
+(when (maybe-require-package 'exec-path-from-shell)
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-setenv "LANG" ""))
 
 (provide 'init-linux)
 ;;; init-linux.el ends here
